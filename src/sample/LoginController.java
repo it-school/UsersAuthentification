ad3a6012e3669d19456bb902d0788fc6f328c516
it-school <GITHUB it@osvita.od.ua>
2021-09-07 20:41:49 +0300

    upd 2021.09

--- src/sample/LoginController.java
+++ src/sample/LoginController.java
@@ -14,33 +14,32 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class LoginController {
-	@FXML
-	TextField login;
-	@FXML
-	PasswordField password;
+   @FXML
+   TextField login;
+   @FXML
+   PasswordField password;
 
-	public void loginClick(ActionEvent actionEvent) {
-		String login = this.login.getText();
-		String password = this.password.getText();
+   public void loginClick(ActionEvent actionEvent) {
+      String login = this.login.getText();
+      String password = this.password.getText();
 
-		if (Main.users.isRegistered(login, password)) {
-			System.out.println("Registered");
-		} else {
-			System.out.println("Not registered");
-		}
-	}
+      if (Main.users.isRegistered(login, password)) {
+         System.out.println("Registered");
+      } else {
+         System.out.println("Not registered");
+      }
+   }
 
-	public void registerClick(ActionEvent actionEvent) throws IOException {
-		Parent root = FXMLLoader.load(getClass().getResource("registerForm.fxml"));
-		Stage stage = new Stage();
-		stage.setTitle("User registration form");
-		stage.setScene(new Scene(root));
-		stage.show();
-	}
+   public void registerClick(ActionEvent actionEvent) throws IOException {
+      Parent root = FXMLLoader.load(getClass().getResource("registerForm.fxml"));
+      Stage stage = new Stage();
+      stage.setTitle("User registration form");
+      stage.setScene(new Scene(root));
+      stage.show();
+   }
 
-
-	public void textViewPressed(KeyEvent keyEvent) {
-		if (keyEvent.getCode() == KeyCode.ENTER)
-			loginClick(new ActionEvent());
-	}
+   public void textViewPressed(KeyEvent keyEvent) {
+      if (keyEvent.getCode() == KeyCode.ENTER)
+         loginClick(new ActionEvent());
+   }
 }
